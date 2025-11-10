f303d95182e52e2cd0bf2adc4250134f3a2a5a2d
Sakshihi-29 <sakshi_25ds096@saitm.ac.in>
2025-11-10 15:21:14 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,14 @@
+a=12
+b=4
+choice=input("enter your choice +,-,*,/")
+if choice=="+":
+ print(a+b)
+elif choice=="-":
+  print(a-b)
+elif choice=="*":
+    print(a*b)
+elif choice=="/":
+  print(a/b)
+else:
+  print("invalid choice")
+                
